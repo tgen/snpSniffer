cf6cefbdd616298b0c994ae42c3dae553ba26388
PedalheadPHX <jkeats@tgen.org>
2021-05-25 17:07:52 -0700

    Added command line options and help message, made dynamic to include or exclude library level stats

--- snpSniffer_Summarize.sh
+++ snpSniffer_Summarize.sh
@@ -2,32 +2,52 @@
 
 # This code will replace the old "Run_SnpSniffer.sh" script
 
-# Requirements:
-# - R with tidyverse available at runtime
-
-# Usage: snpSniffer_Summarize.sh </Full/Path/Template_Database.ini>
-
-# Check that the required file was provided at the command line
-if [[ $# -ne 1 ]]
-then
-  echo -e "Expected 1 arguments, found $#"
-  echo -e "Usage: snpSniffer_Summarize.sh </Full/Path/Template_Database.ini>"
-  exit 1
-fi
-
-###############################################
-## Define Required Variables
-###############################################
-
-# The required JAVA -jar and R Script files will be in the same folder as this script, detect dynamically
+# Capture the current script path to dynamically link default input database.ini file
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 SCRIPT_DIR=`dirname ${SCRIPT_PATH}`
 
+## Usage help message
+usage()
+{
+  # `cat << EOF` This means that cat should stop reading when EOF is detected
+  cat << EOF
+
+  Usage: $0 [ options ]
+
+  -h      Display Help
+
+  Required Options
+  -d      Input Database File (databaseV5_hg38_ucsc.ini)
+  -f      Filter Library Level Results (Yes/No) [Yes]
+
+  NOTES:  Default database supports human GRCh38/hg38 coordinates WITHOUT chr prefixes
+
+EOF
+# EOF is found above and hence cat command stops reading. This is equivalent to echo but much neater when printing out.
+  exit 2
+}
+
+## Assign default values
+DATABASE=${SCRIPT_DIR}/grch38_hg38_ucsc_contigs/databaseV5_hg38_ucsc.ini
+FILTER=Yes
+
+## Capture and assign variables from inputs
+while getopts 'd:e:?h' flag
+do
+    case ${flag} in
+        d) VCF=${OPTARG};;
+        f) DATABASE=${OPTARG};;
+        h|?) usage;;
+    esac
+done
+
+
+###############################################
+## Define Paths to Required scripts based on location of initial script
+###############################################
+
 SNP_SNIFFER_JAR=${SCRIPT_DIR}/snpSniffer.jar
-
 SNP_SNIFFER_GRAPH=${SCRIPT_DIR}/snpSniffer_Summarize.R
-
-EMPTY_DATABASE=$1
 
 ###############################################
 ## Collect available genotype results and add to fresh database
@@ -46,13 +66,27 @@ echo
 echo "Creating snpSniffer Summary of Current Folder"
 
 # Create an empty version of the database
-cat ${EMPTY_DATABASE} > SnpSniffer_DB.ini
+cat ${DATABASE} > SnpSniffer_DB.ini
 
 # Find all the pre-calculated genotype results (search for all expected extensions)
 # Drop the library level results for simplified viewing
-find . -type f -name "*flt.vcf" ! -name "*[A-Z][0-9][0-9][0-9][0-9][0-9]*" > Temp_SnpSniffer_Genotype_Paths.txt
-find . -type f -name "*snpsniffer.vcf" ! -name "*[A-Z][0-9][0-9][0-9][0-9][0-9]*" >> Temp_SnpSniffer_Genotype_Paths.txt
-find . -type f -name "*snpSniffer.vcf" ! -name "*[A-Z][0-9][0-9][0-9][0-9][0-9]*" >> Temp_SnpSniffer_Genotype_Paths.txt
+if [ $FILTER == "Yes" ]
+then
+  find . -type f -name "*flt.vcf" ! -name "*[A-Z][0-9][0-9][0-9][0-9][0-9]*" > Temp_SnpSniffer_Genotype_Paths.txt
+  find . -type f -name "*snpsniffer.vcf" ! -name "*[A-Z][0-9][0-9][0-9][0-9][0-9]*" >> Temp_SnpSniffer_Genotype_Paths.txt
+  find . -type f -name "*snpSniffer.vcf" ! -name "*[A-Z][0-9][0-9][0-9][0-9][0-9]*" >> Temp_SnpSniffer_Genotype_Paths.txt
+elif [ $FILTER == "No" ]
+  find . -type f -name "*flt.vcf" > Temp_SnpSniffer_Genotype_Paths.txt
+  find . -type f -name "*snpsniffer.vcf" >> Temp_SnpSniffer_Genotype_Paths.txt
+  find . -type f -name "*snpSniffer.vcf" >> Temp_SnpSniffer_Genotype_Paths.txt
+else
+  echo
+  echo "------------------------"
+  echo ERROR incorrect value provided to option -f, only Yes or No are expected
+  echo
+  echo
+  exit 1
+fi
 
 # Add genotypes to the database
 for line in `cat Temp_SnpSniffer_Genotype_Paths.txt`
